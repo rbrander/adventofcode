1b00f087e08d3efd57c508b952111ae694d15c35
Rob Brander <rob.brander@telus.com>
2020-12-20 12:22:47 -0500

    Updated script

--- 2020/script.sh
+++ 2020/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir $1
 cd $1
-echo "module.exports = \`\`;" > data.js
-echo "const data = require('./data');" > answer1.js
+echo "module.exports = \`\`" > data.js
+echo "const data = require('./data')" > answer1.js
 touch answer2.js
 code .
