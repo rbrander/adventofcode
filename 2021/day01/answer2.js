e8201ae06a7f8c0985d09f20ef8ca88ad7539f20
Rob Brander <rob.brander@telus.com>
2021-12-06 19:16:44 -0500

    [2021] Day 1

---
+++ 2021/day01/answer2.js
@@ -0,0 +1,61 @@
+/*
+--- Part Two ---
+
+Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+
+Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+
+199  A
+200  A B
+208  A B C
+210    B C D
+200  E   C D
+207  E F   D
+240  E F G
+269    F G H
+260      G H
+263        H
+
+Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+
+Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+In the above example, the sum of each three-measurement window is as follows:
+
+A: 607 (N/A - no previous sum)
+B: 618 (increased)
+C: 618 (no change)
+D: 617 (decreased)
+E: 647 (increased)
+F: 716 (increased)
+G: 769 (increased)
+H: 792 (increased)
+
+In this example, there are 5 sums that are larger than the previous sum.
+
+Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
+*/
+
+// Pick your data source
+const data = require('./data');
+// const data = require('./testData');
+
+// convert the data (text blob) into an array of numbers
+const heights = data.split('\n').map(Number);
+
+// Slide that window along and capture the sum of the windows as they pass
+let sums = []
+for (let i = 0; i < heights.length - 2; i++) {
+  const window = [heights[i], heights[i+1], heights[i+2]];
+  const sum = window.reduce((a, b) => a + b);
+  sums.push(sum);
+}
+
+// Count the number of increases
+let numIncreases = 0;
+for (let i = 1; i < sums.length; i++) {
+  const height = sums[i];
+  const prevHeight = sums[i-1];
+  numIncreases += height > prevHeight ? 1 : 0;
+}
+console.log('Number of increases:', numIncreases); // 1257
